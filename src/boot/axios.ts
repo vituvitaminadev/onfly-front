2df8a37553725963abfe0148951989fda3b3cab9
Vitor Souza <79655309+vituvitaminadev@users.noreply.github.com>
2024-02-06 19:23:48 -0300

    fix base url for the api

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -14,7 +14,7 @@ declare module '@vue/runtime-core' {
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: 'https://localhost:8000/api' });
+const api = axios.create({ baseURL: 'http://localhost:8000/api' });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('userToken');
